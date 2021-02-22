5cbc311b88c7a0edc51ce3ac5f38404d8389e63e
Finn Tegeler <developing@finn-tegeler.de>
2021-02-22 18:55:05 +0100

    Change entity and add additional entity

--- src/main/java/com/foo/TestEntity.java
+++ src/main/java/com/foo/TestEntity.java
@@ -1,7 +1,5 @@
 package com.foo;
 
-import java.util.UUID;
-
 import org.bson.types.ObjectId;
 
 import dev.morphia.annotations.Entity;
@@ -9,20 +7,20 @@ import dev.morphia.annotations.Id;
 import dev.morphia.annotations.Reference;
 
 @Entity
-public class MyEntity {
+public class TestEntity {
 
     @Id
     public ObjectId id;
 
     @Reference
-    public TestEntity reference;
+    public MyEntity backReference;
 
     /**
      * Sets the id and returns the object without setting
      * @return the object
      */
-    public static MyEntity getDefault() {
-        MyEntity entity = new MyEntity();
+    public static TestEntity getDefault() {
+        TestEntity entity = new TestEntity();
 
         entity.id = ObjectId.get();
 
